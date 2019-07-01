0458d6b3df7891617adc97851b1e7696409f4f15
rashadhrrs <rashadhrrs@gmail.com>
2019-07-01 17:48:22 +0700

    perubahan query getByName

--- src/main/java/com/eksad/latihanjpa/dao/EmployeeDAOImpl.java
+++ src/main/java/com/eksad/latihanjpa/dao/EmployeeDAOImpl.java
@@ -57,9 +57,9 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
 	@Override
 	public List<Employee> getByName(String name) {
-		// 		return entityManager.createNativeQuery("select * from employee where name like '%?0%'", Employee.class)
+		return entityManager.createNativeQuery("select * from employee where name like '%?0%'", Employee.class)
 
-		return entityManager.createQuery("select e from Employee e where e.name like ?0 ", Employee.class)
+		//return entityManager.createQuery("select e from Employee e where e.name like ?0 ", Employee.class)
 		.setParameter(0, "%" + name + "%")
 		.getResultList();
 	}
